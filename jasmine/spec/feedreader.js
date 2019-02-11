a4c2ba1b978097f92a2444b3ff1ffa81ff4b126d
Raphaelli Bellini <rafael.bellini@hotmail.com>
2019-02-11 17:25:31 -0200

    Add a test to see if you have a url defined in each feed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('url defined', function() {
+            for(const feed of allFeeds) {
+                let feedUrl = feed.url;
+                expect(feedUrl).toBeDefined();
+                expect(feedUrl.length).not.toBe(0);
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
